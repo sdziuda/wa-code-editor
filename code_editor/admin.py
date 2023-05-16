9e6ee7b9bd53acfee5b16cb4340c4e7d62870276
sdziuda <szymondziuda95@gmail.com>
2023-05-16 23:03:31 +0200

    Improved AppUser creation

--- code_editor/admin.py
+++ code_editor/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
-
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User, Group
 from .models import Directory, File, Section, AppUser
 
 admin.site.register(Section)
-admin.site.register(AppUser)
+
+
+class AppUserInline(admin.TabularInline):
+    model = AppUser
+    can_delete = False
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [AppUserInline]
+
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
+admin.site.register(User, UserAdmin)
 
 
 class SectionInline(admin.TabularInline):
